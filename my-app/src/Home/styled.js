a0db8b6a6b9e5a31d5221eae5d14611a3fb88616
mini <ssh0040233@gmail.com>
2023-10-06 23:27:59 +0900

    진행중

---
+++ my-app/src/Home/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const HomeContainerWrapper = styled.div`
+  display: flex;
+  background-color: yellowgreen;
+  height: 100%;
+`;
+
+export const HomeLeftContainer = styled.div`
+  display: flex;
+  background-color: #61dafb;
+  width: 30%;
+`;
+
+export const HomeRightContainer = styled.div`
+  display: flex;
+  background-color: thistle;
+  width: 70%;
+`;
