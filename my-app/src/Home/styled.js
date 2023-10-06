35e7f61bd59d0b1aabc0e92e040745ad1d010541
mini <ssh0040233@gmail.com>
2023-10-07 00:07:28 +0900

    작업중

--- my-app/src/Home/styled.js
+++ my-app/src/Home/styled.js
@@ -3,17 +3,36 @@ import styled from 'styled-components';
 export const HomeContainerWrapper = styled.div`
   display: flex;
   background-color: yellowgreen;
-  height: 100%;
+  height: 900px;
+`;
+
+export const ProfileImgContainer = styled.div`
+  height: 40%;
+  padding: 10px;
 `;
 
 export const HomeLeftContainer = styled.div`
-  display: flex;
   background-color: #61dafb;
   width: 30%;
+  height: 100%;
 `;
 
 export const HomeRightContainer = styled.div`
   display: flex;
   background-color: thistle;
   width: 70%;
+  height: 100%;
+`;
+
+export const RightBox = styled.div`
+    width: 50%;
+    height: 100px;
+    background-color: yellow;
+`;
+
+export const TitleBox = styled.div`
+    height: 10%;
+    font-size: x-large;
+    font-weight: bold;
+    padding: 20px;
 `;
